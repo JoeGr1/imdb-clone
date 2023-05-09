a2f9909e67fddab14cd29186eab6efe2da00aa02
Joe Gregson <joegregson@icloud.com>
2023-05-09 17:18:43 +0100

    create header with some styling

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -6,15 +7,16 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "JG-imdb clone",
   description: "Learn next.13",
-  // icons: {
-  //   icon: "/favicon.ico",
-  // },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+
+        {children}
+      </body>
     </html>
   );
 }
