081d9fd3db672c09f9666172b6477e488cd0aac3
Joe Gregson <joegregson@icloud.com>
2023-05-11 23:09:58 +0100

    add movie page

---
+++ src/app/error 2.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect } from "react";
+
+const Error = ({ error, reset }) => {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+  return (
+    <div className="text-center mt-10">
+      <h1>Something Went Wrong!</h1>
+      <button
+        className="text-amber-600"
+        onClick={() => {
+          reset();
+        }}
+      >
+        Try Again
+      </button>
+    </div>
+  );
+};
+export default Error;
---
+++ src/app/loading 2.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="felx justify-center">
+      <img className="h-96" src="Spinner.svg" alt="Loading..." />
+    </div>
+  );
+};
+
+export default Loading;
